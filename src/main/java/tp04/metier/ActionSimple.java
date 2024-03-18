a61c7e4b629aaf812a51d47ea6896c5486b6805d
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2024-03-18 14:59:42 +0100

    Fatima/Yasine_US 3 - Acheter action simple

--- src/main/java/tp04/metier/ActionSimple.java
+++ src/main/java/tp04/metier/ActionSimple.java
@@ -9,15 +9,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * Représente une action simple.
+ * Une action simple est une action avec un libellé et des cours enregistrés pour différents jours.
+ * Cette classe hérite de la classe abstraite Action.
+ * 
  * @author somebody
  */
 public class ActionSimple extends Action {
 
-    // attribut lien
+    // Map pour stocker les cours pour chaque jour
     private Map<Jour, Cours> mapCours;
 
-    // constructeur
+    
+    /**
+     * Constructeur de la classe ActionSimple.
+     * Initialise une nouvelle instance d'une action simple avec le libellé spécifié.
+     * 
+     * @param libelle Le libellé de l'action simple.
+     */
+    
+    
     public ActionSimple(String libelle) {
         // Action simple initialisée comme 1 action
         super(libelle);
@@ -25,7 +36,15 @@ public class ActionSimple extends Action {
         this.mapCours = new HashMap();
     }
 
-    // enrg possible si pas de cours pour ce jour
+    
+    /**
+     * Enregistre un cours pour un jour spécifique.
+     * Si aucun cours n'est enregistré pour le jour donné, un nouveau cours est créé avec la valeur spécifiée.
+     * 
+     * @param j Le jour pour lequel enregistrer le cours.
+     * @param v La valeur du cours.
+     */
+    
     public void enrgCours(Jour j, float v) {
         if (this.mapCours.containsKey(j) == false) {
             this.mapCours.put(j, new Cours(j, v));
@@ -34,9 +53,12 @@ public class ActionSimple extends Action {
 
     @Override
     public float valeur(Jour j) {
+       // Vérifie si un cours est enregistré pour le jour donné
         if (this.mapCours.containsKey(j) == true) {
+            // Si un cours est trouvé, retourne sa valeur
             return this.mapCours.get(j).getValeur();
         } else {
+            // Si aucun cours n'est trouvé, retourne 0
             return 0; // definition d'une constante possible
         }
     }
